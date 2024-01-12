8e7539b67ef3a136cd416eccd416166610631844
houjinlong <houjinlong2014@xiaochuankeji.cn>
2024-01-12 10:25:10 +0800

    ImageInput增加图片预览

--- packages/component/src/ReactJsonschemaForm/widgets/ImageInput/index.tsx
+++ packages/component/src/ReactJsonschemaForm/widgets/ImageInput/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import { Input,Upload} from 'antd';
+import React,{PropsWithChildren} from 'react';
+import { Input,Upload,Popover,Image} from 'antd';
 import * as Style from './style';
 import {WidgetProps} from '@rjsf/core';
 import { IconWidget } from '../../../IconWidget';
+export const AddonWarp = (props: PropsWithChildren<unknown>) => {
+  return (
+    <>
+      <div
+        style={{
+          width: '1em',
+        }}
+      ></div>
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          cursor: 'pointer',
+          fontSize: '20px',
+        }}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
+
 export const ImageInput: React.FC<WidgetProps> = (props) => {
   let {customRequest,...otherUploadProps} = {
     ...props.formContext.UploadProps,
@@ -15,6 +43,36 @@ export const ImageInput: React.FC<WidgetProps> = (props) => {
     <Style.Box>
       <Input
         addonBefore={<Upload accept="image/*" showUploadList={false} {...otherUploadProps} ><IconWidget icon="CloudUpload"  title='点击上传' /></Upload>} 
+        addonAfter={
+          <AddonWarp>
+            {props.value ? (
+              <Popover
+                content={
+                  <Image
+                    src={props.value}
+                    style={{
+                      maxHeight: '300px',
+                      maxWidth: '300px',
+                    }}
+                  />
+                }
+              >
+                <Image
+                  src={props.value}
+                  preview={false}
+                  style={{
+                    display: 'block',
+                    margin: '5px',
+                    maxHeight: '20px',
+                    maxWidth: '20px',
+                  }}
+                />
+              </Popover>
+            ) : (
+              <IconWidget icon="Imgage" title={"图片预览"}></IconWidget>
+            )}
+          </AddonWarp>
+        }
         value={props.value}
         onChange={(e) => {
           props.onChange?.(e.target.value);
